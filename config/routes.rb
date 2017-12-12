b364141ee2b48f5aa4e032816baa22c7307cbd08
seoulstice <jungyoungjoon@gmail.com>
2017-12-11 20:03:25 -0700

    created migration file for categories and made routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
+  resources :categories
   resources :companies do
     resources :jobs
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
