775bc1328275980399f5d6dc7279b33cfe775f7a
seoulstice <jungyoungjoon@gmail.com>
2017-12-14 02:23:51 -0700

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
   end
 
+  get '/dashboard', to: 'dashboard#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
